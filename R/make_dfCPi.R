92db6126a5d087c1c69cd36c8309e623ea4b1a46
christiaanpauw <christiaan.pauw@nova.org.za>
2024-09-03 19:04:48 +0200

    Inference to total duration of MP

--- R/make_dfCPi.R
+++ R/make_dfCPi.R
@@ -24,6 +24,8 @@
 make_dfCPi <- function (dfConMon = dfConMon,
                         KTresults = dfresBP,
                         indexvars = c("place", "year",  "fuel"),
+                        MPvar = "Period",
+                        durationVar = "Duration",
                         startdate = "date_start_monitoring",
                         enddate = "date_end_monitoring",
                         N = 19,
@@ -35,13 +37,14 @@ make_dfCPi <- function (dfConMon = dfConMon,
                         web3 = FALSE)
 
 {
-dfConMon_sum <- dfConMon %>% group_by(!!!syms(indexvars)) %>%
-    summarise(date_start_monitoring_period = min(!!sym(startdate)),
-              date_end_monitoring_period = max(!!sym(enddate)),
-              project_days = sum(!!sym(ndays_cm), na.rm = TRUE),
+dfConMon_sum <- dfConMon %>% group_by(!!!syms(c(indexvars, MPvar))) %>%
+    summarise({{startdate}} := min(!!sym(startdate)),
+              {{enddate}} := max(!!sym(enddate)),
+              duration = mean(!!sym(durationVar)),
+              obs_days = sum(!!sym(ndays_cm), na.rm = TRUE),
               av_days = mean(!!sym(ndays_cm), na.rm = TRUE),
               frMi. = sum(!!sym(nfire_cm),na.rm = TRUE),
-              frMi = frMi./as.numeric(project_days))
+              frMi = frMi./as.numeric(obs_days))
 
   dfKTresults <- KTresults %>% ungroup()
   dfKTresults <- dfKTresults %>%  select(!!XBij, !!XMij, !!!indexvars) %>%
@@ -49,8 +52,10 @@ dfConMon_sum <- dfConMon %>% group_by(!!!syms(indexvars)) %>%
     summarise(XMi = mean(!!sym(XMij), na.rm = TRUE),
               XBi = mean(!!sym(XBij), na.rm = TRUE))
 
-  dfCPi <- dfConMon_sum %>% left_join(dfKTresults) %>%
-    mutate(CPi = as.numeric(av_days)*N*frMi * XMi,
+  dfCPi <- dfConMon_sum %>%
+    left_join(dfKTresults) %>%
+    mutate(N = N,
+           CPi = as.numeric(duration) * N * frMi * XMi,
            CPi = units::as_units(CPi, "kg"),
            CPi = units::set_units(CPi, "tonne"))
   if (is.null(format) & !web3){
